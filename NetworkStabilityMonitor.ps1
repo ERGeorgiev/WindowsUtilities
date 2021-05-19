8a861ca1eeee3344e4fffb870292bf915ffb7cce
Eduard Georgiev <e.r.georgiev@gmail.com>
2021-05-20 00:50:48 +0300

    Refactor NSM

--- NetworkStabilityMonitor.ps1
+++ NetworkStabilityMonitor.ps1
@@ -6,11 +6,13 @@ $netRoute = Get-NetRoute |
 	Where {$_.DestinationPrefix -eq '0.0.0.0/0'}
 $router = $netRoute.NextHop[0]
 $google = '8.8.8.8'
+$cloudflare = '1.1.1.1'
 
-Write-Host "Monitoring: $router"
+Write-Host "Router: $router"
 Write-Host "Monitoring: $google"
+Write-Host "Monitoring: $cloudflare"
 
-$init = {	
+$init = {
 	Function Monitor-Connection {
 		Param ([String]$name, [String]$ip)
 		$conn = Test-Connection -TargetName $ip -IPv4 -Count 1
@@ -27,12 +29,25 @@ $init = {
 	}
 }
 
-Do
-{
-	Start-Job -InitializationScript $init -Name "MonitorGoogle" -ScriptBlock{Monitor-Connection "Google" $args[0]} -ArgumentList $google | Out-Null
-	Start-Job -InitializationScript $init -Name "MonitorGoogle" -ScriptBlock{Monitor-Connection "Router" $args[0]} -ArgumentList $router | Out-Null
-	Get-Job | Wait-Job | Out-Null
-	Get-Job | Receive-Job
-	Get-Job | Remove-Job
+Do {
+	$conn = Test-Connection -TargetName $router -IPv4 -Count 1
+	$lat = $conn.Latency
+	$status = $conn.Status
+	if ($status -ne 'Success') {
+		$date = Get-Date
+		Write-Warning "[$date] Unable to reach router ($status)"
+	}
+	elseif ($lat -gt 100) {
+		$date = Get-Date
+		Write-Warning "[$date] High latency for router ($lat)"
+	}
+	else {
+		Start-Job -InitializationScript $init -ScriptBlock{Monitor-Connection "Google" $args[0]} -ArgumentList $google | Out-Null
+		Start-Job -InitializationScript $init -ScriptBlock{Monitor-Connection "CloudFlare" $args[0]} -ArgumentList $cloudflare | Out-Null
+		Get-Job | Wait-Job | Out-Null
+		Get-Job | Receive-Job
+		Get-Job | Remove-Job
+	}
+	
 	Sleep 1
 } While ($true)
