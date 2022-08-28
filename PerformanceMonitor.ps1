ef42574068f713b9214a5f68d324930052302756
Eduard Georgiev <e.r.georgiev@gmail.com>
2022-08-28 10:03:35 +0100

    Rewrote and improved Performance Monitor

--- PerformanceMonitor.ps1
+++ PerformanceMonitor.ps1
@@ -1,32 +1,67 @@
 #Requires -Version 7
+$previousProcesses = @{}
+$initialized = $false
+$period = 5
 
-Write-Host "Monitoring CPU..."
+Write-Host "Performance Monitor" -BackgroundColor White -ForegroundColor Black
+Write-Host "Every $period second(s), any started/stopped or high usage processes will be logged here."
+$cpuThreads = (Get-CimInstance -ClassName Win32_Processor).NumberOfLogicalProcessors
+$cpuSecondsPerDelay = $cpuThreads * $period
+$cpuUsagePercentageThreshold = 5
 
 Do
 {
-	$countersGrouped = (Get-Counter '\Process(*)\% Processor Time' 2>&1).CounterSamples | Group-Object -Property InstanceName
-	$counters = @{}
-	$countersGrouped | ForEach-Object -Process {$counters += @{$_.name = ($_.Group | Measure-Object -Sum {$_.CookedValue}).Sum}}
-	$idle = $counters["idle"]
-	$total = $counters["_total"]
-	$used = $total - $idle
-	$usedPct = ($used / $total) * 100
-	if ($usedPct -gt 50) {
-		$usedPct = [Math]::Round($usedPct, 2)
-		$date = Get-Date
-		
-		$counters = $counters.GetEnumerator() | Where-Object {$_.Key -ne "idle" -and $_.Key -ne "_total"}
-		$top = $counters | Sort-Object -Property Value -Descending | Select-Object -First 3
-		[Array]$topArray = @()
-		Foreach ($t in $top) {
-			$val = ($t.Value / $total) * 100
-			$val = [Math]::Round($val, 2)
-			$name = $t.Name
-			$topArray += "$name[$val%]"
+	$date = Get-Date
+	$allProcesses = Get-Process
+	
+	# Combine same process names into the same value
+	$currentProcesses = @{}
+	Foreach ($p in $allProcesses) {
+		if ($currentProcesses.Keys -contains $p.Name) {
+			$currentProcesses[$p.Name] += $p.CPU
 		}
-		$topString = $topArray -Join ', '
-		
-		Write-Host "[$date] High CPU usage: $usedPct% ($topString)"
+		else {
+			$currentProcesses[$p.Name] = $p.CPU
+		}
 	}
-	Sleep 5
+	
+	# Get any stopped processes
+	Foreach ($p in $previousProcesses.GetEnumerator()) {
+		$name = $p.Key
+		if (($currentProcesses.Keys -contains $name) -eq $false) {
+			Write-Host "[$date] Process Stopped: $name" -ForegroundColor Red
+		}
+	}
+	
+	# Get any outliers and started processes
+	$spikingProcesses = @{}
+	Foreach ($p in $currentProcesses.GetEnumerator()) {
+		$name = $p.Key
+		$current = $currentProcesses[$name]
+		if ($previousProcesses.Keys -contains $name) {
+			$change = $current - $previousProcesses[$name]
+			$usage = ($change / $cpuSecondsPerDelay) * 100
+			if ($usage -gt $cpuUsagePercentageThreshold) {
+				$usage = [Math]::Min($usage, 100)
+				$spikingProcesses[$name] = [Int]$usage
+			}
+		}
+		else {
+			if ($initialized) {
+				Write-Host "[$date] Process Started: $name" -ForegroundColor Green
+			}
+		}
+	}
+
+	# Print outliers
+	Foreach ($p in $spikingProcesses.GetEnumerator()) {
+		$name = $p.Key
+		$usage = $p.Value
+		Write-Host "[$date] High CPU Usage: $name ($usage%)"
+	}
+	
+	$previousProcesses = $currentProcesses
+	$initialized = $true
+	
+	Start-Sleep $period
 } While ($true)
